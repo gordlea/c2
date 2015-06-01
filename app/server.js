3b0c8de2b58e40d585f3919ceab8c53662903ba6
Gord Lea <glea@ddswireless.com>
2015-06-01 10:45:22 -0700

    Added primus

--- app/server.js
+++ app/server.js
@@ -2,12 +2,13 @@
 var SocketIO = require('socket.io');
 var config = require('config');
 var Hapi = require('hapi');
+var Primus = require("primus");
 var mongoose = require('mongoose');
 var winston = require('winston');
 var _ = require('lodash');
 var log = require('./log.js').getFor('server');
 
-// var log = winston.loggers.get('server');
+var remoteServices = new Map();
 
 class Server {
     constructor() {
@@ -28,9 +29,25 @@ class Server {
             .then(() => {
                 log.info('Server running at:', this.server.info.uri);
                 log.debug('Setting up api handlers:');
+
+                log.debug('Setting up websockets');
+                this._startSocketEndpoint();
+
             }).catch(function(error) {
                 log.error(error.stack);
             });
+    }
+
+    _startSocketEndpoint() {
+        this.primus = new Primus(this.server.listener, {});
+
+        /*
+         * TODO go through list of existing services in db and check if they are alive,
+         * and let them know we are back
+         */ 
+        this.primus.on('connection', function (spark) {
+            spark.write('Hello, world!');
+        });
     }
 
     _startWebEndpoint() {
